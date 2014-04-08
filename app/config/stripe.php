f49b0b089b57148326c58292c62d336f0568a764
Ken Priest <ken@priest210.com>
2014-04-08 16:30:02 -0500

    Added new stripe.php files in config and newly created development folder.

---
+++ app/config/stripe.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+	'secret_key' => ' paste secret live key here ',
+	'publishable_key' => ' paste publishable live key here '
+];
+
---
+++ app/development/stripe.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+	'secret_key' => ' paste secret test key here ',
+	'publishable_key' => ' paste publishable test key here '
+];

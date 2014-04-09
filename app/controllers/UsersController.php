eb611e8be7ffd329cb5f3e6753adc139ba50352a
lesssliemarie <lesssliemarie@gmail.com>
2014-04-09 16:30:13 -0500

    Made comments and code for UsersController

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -9,7 +9,8 @@ class UsersController extends \BaseController {
 	 */
 	public function index()
 	{
-		// view to show all users ADMIN ONLY
+		// if ADMIN show view of users
+		// else show access denied
 	}
 
 	/**
@@ -30,6 +31,7 @@ class UsersController extends \BaseController {
 	public function store()
 	{
 		// grab form input and send to DB users table
+		// redirect to UsersController@show with their new id
 	}
 
 	/**
@@ -41,7 +43,7 @@ class UsersController extends \BaseController {
 	public function show($id)
 	{
 		$userInfo = User::findOrFail($id);
-		return View::make('account')->with('userInfo' as $userInfo);
+		return View::make('account')->with('userInfo', $userInfo);
 	}
 
 	/**
@@ -52,7 +54,8 @@ class UsersController extends \BaseController {
 	 */
 	public function edit($id)
 	{
-		//
+		$userInfo = User::findOrFail($id);
+		return View::make('edit-account')->with('userInfo', $userInfo);
 	}
 
 	/**
@@ -63,7 +66,9 @@ class UsersController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		// grab input and save changes to users table (same as create)
+		// redirect to account with updated info and success message
+		// if error, flash error message
 	}
 
 	/**
@@ -74,7 +79,9 @@ class UsersController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		//
+		// delete account button to account view
+		// delete record from users table in DB
+		// flash success message once deleted, error if not
 	}
 
 }

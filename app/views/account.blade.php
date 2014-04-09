ea22b0882bf0d23a96796d8fe8a0771de25e8771
lesssliemarie <lesssliemarie@gmail.com>
2014-04-09 09:26:52 -0500

    Editing user account page

--- app/views/account.blade.php
+++ app/views/account.blade.php
@@ -4,25 +4,26 @@
 
 		<h1>Good to see you again, {{{ Auth::user()->first_name }}}!</h1>
 
+		<h3>Care packages you've sent:</h3>
 		<table class="table table-bordered">
 			<tr>
-				<td>Customer Name:</td>
+				<th>Customer Name:</th>
 				<td>{{{ Auth::user()->first_name }}} {{{ Auth::user()->last_name }}}</td>
 			</tr>
 			<tr>	
-				<td>Email:</td>
+				<th>Email:</th>
 				<td>{{{ Auth::user()->email }}}</td>
 			</tr>
 			<tr>
-				<td>Phone Number:</td>
+				<th>Phone Number:</th>
 				<td>{{{ Auth::user()->email }}}</td>
 			</tr>
 			<tr>
-				<td>Member Since:</td>
+				<th>Member Since:</th>
 				<td>{{{ Auth::user()->created_at->format('l, F jS, Y') }}}</td>
 			</tr>
 			<tr>
-				<td>Last Profile Update: </td>
+				<th>Last Profile Update: </th>
 				<td>{{{ Auth::user()->updated_at->format('l, F jS, Y') }}}</td>
 			</tr>
 		</table>

7a4cfe2edafb0d05e5d999d9e179d25099097226
lesssliemarie <lesssliemarie@gmail.com>
2014-04-11 17:09:04 -0500

    Simplified DB migrations

--- app/database/migrations/2014_04_09_183641_Initial_table_migration.php
+++ app/database/migrations/2014_04_09_183641_Initial_table_migration.php
@@ -23,22 +23,8 @@ class InitialTableMigration extends Migration {
 		    $table->string('phone', 15)->nullable();     			
 		    $table->string('stripe_customer_token',100)->nullable();
 		    $table->boolean('is_admin');					
-		    $table->timestamps();		
-		});
-
-		Schema::create('orders', function($table)
-		{
-		    $table->increments('id')->unsigned();
-		    $table->integer('user_id')->unsigned();
-		    $table->foreign('user_id')->references('id')->on('users');
-		    $table->string('recipient_name', 100);
-		    $table->string('street', 100);
-		    $table->string('city', 100);
-		    $table->string('state', 100);
-		    $table->string('zip', 10); 				
-		    $table->string('gift_message');  				
-		    $table->string('stripe_transaction_token', 100);	
 		    $table->timestamps();
+		    $table->softDeletes();		
 		});
 
 		Schema::create('package_types', function($table)
@@ -47,20 +33,28 @@ class InitialTableMigration extends Migration {
 		    $table->string('description', 200);
 		    $table->decimal('price', 5, 2);
 		    $table->timestamps();
+		    $table->softDeletes();
 
 		});
 
-		Schema::create('packages', function($table)
+		Schema::create('orders', function($table)
 		{
 		    $table->increments('id')->unsigned();
-		    $table->string('description', 200);
-		    $table->integer('order_id')->unsigned();
-		    $table->foreign('order_id')->references('id')->on('orders');
-		    $table->integer('package_type_id')->unsigned()->references('id')->on('package_types');
+		    $table->integer('user_id')->unsigned();
+		    $table->foreign('user_id')->references('id')->on('users');
+		   	$table->integer('package_type_id')->unsigned();
 			$table->foreign('package_type_id')->references('id')->on('package_types');
-			$table->timestamp('packaged_at')->nullable(); 
-		    $table->timestamp('delivered_at')->nullable();
-		    $table->timestamps();  
+		    $table->string('recipient_name', 100);
+		    $table->string('street', 100);
+		    $table->string('city', 100);
+		    $table->string('state', 100);
+		    $table->string('zip', 10); 				
+		    $table->string('gift_message', 500);
+		    $table->timestamp('packaged_at')->nullable(); 
+		    $table->timestamp('delivered_at')->nullable();  				
+		    $table->string('stripe_transaction_token', 100);	
+		    $table->timestamps();
+		    $table->softDeletes();
 		});
 
 	}
@@ -72,19 +66,14 @@ class InitialTableMigration extends Migration {
 	 */
 	public function down()
 	{
-		Schema::table('packages', function($table)
-		{
-			$table->dropForeign('packages_order_id_foreign');
-			$table->dropForeign('packages_package_type_id_foreign');
-		});
 
 		Schema::table('orders', function($table)
 		{
 			$table->dropForeign('orders_user_id_foreign');
+			$table->dropForeign('orders_package_type_id_foreign');
 		});
 
 		Schema::drop('package_types');
-		Schema::drop('packages');
 		Schema::drop('orders');
 		Schema::drop('users');
 	}

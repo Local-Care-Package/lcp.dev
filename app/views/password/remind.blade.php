e578042ba4b45c623989caea86497c6f94b59cd9
lesssliemarie <lesssliemarie@gmail.com>
2014-04-13 17:50:45 -0500

    Added form styles for password remind page

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,5 +1,22 @@
-<form action="{{ action('RemindersController@postRemind') }}" method="POST">
-	<input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <input type="email" name="email">
-    <input type="submit" value="Send Reminder">
+@extends('layouts.master')
+
+@section('main-content')
+<h1>Can't remember your password? <small>No problem! We'll help with that.</small></h1>
+
+<form class="form-horizontal" action="{{ action('RemindersController@postRemind') }}" method="POST">
+	<div class="form-group">
+		<input type="hidden" name="_token" value="{{ csrf_token() }}">
+		<div class="col-md-6">
+			<div class="col-sm-3">
+	    		<label for="email" class="control-label">Email Address:</label>
+	    	</div>
+    		<div class="col-sm-9">
+    			<input type="email" name="email" id="email" class="form-control"><br>
+    		</div>
+    		<div class="col-sm-offset-3 col-sm-9">
+    			<input type="submit" class="btn btn-sm" value="Send Password Reset Email">
+    		</div>
+    	</div>
+    </div>
 </form>
+@stop

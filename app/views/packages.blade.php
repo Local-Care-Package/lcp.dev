e978df339d67ccbae0889b9d387af2df3fe8a774
lesssliemarie <lesssliemarie@gmail.com>
2014-04-14 10:26:46 -0500

    Pulls package type data from DB, added details

--- app/views/packages.blade.php
+++ app/views/packages.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.master')
 
 @section('main-content')
-
 		<!-- HERO IMAGE -->
 		<div id="hero">
 			<div id="hero-message">
 				<h1>High quality products with a personal touch</h1>
 				<p>Blah blah blah blah</p>
-				<span><a role="button" class="btn btn-sm" id="goToPackages" href="#packages">Pick a package!</a></button>
 			</div>
 		</div>
 		<!-- END HERO -->
@@ -15,46 +13,42 @@
 		<div id="packages" class="row">
 			<div class="col-md-4">
 				<img src="/img/package2.jpg">
-				<h3>Package Description</h3>
+				<h3>{{{ $packages[0]->description }}} – ${{{ $packages[0]->price }}}</h3>
 					<ul>
-						<li>Price</li>
-						<li>Price</li>
-						<li>Price</li>
+						<li>6 Bakery Lorraine Macarons</li>
+						<li>Dinner for two at Barbaro</li>
+						<li>Coffee for two at Local Coffee</li>
+						<li>One-hour bike ride for two with Bcycle</li>
 					</ul>
 				<span><a role="button" class="btn btn-sm" href="">Send Package</a></span>
 			</div>
 			<div class="col-md-4">
 				<img src="/img/package2.jpg">
-				<h3>Package Description</h3>
+				<h3>{{{ $packages[1]->description }}} – ${{{ $packages[1]->price }}}</h3>
 					<ul>
-						<li>Price</li>
-						<li>Price</li>
-						<li>Price</li>
+						<li>6 Bakery Lorraine Macarons</li>
+						<li>6 cookies from Lily's Cookies</li>
+						<li>Dinner for two at Barbaro</li>
+						<li>Dinner for two at The Monterey</li>
+						<li>Coffee for two at Local Coffee</li>
+						<li>One-hour bike ride for two with Bcycle</li>
 					</ul>
 				<span><a role="button" class="btn btn-sm" href="">Send Package</a></span>
 			</div>
 			<div class="col-md-4">
 				<img src="/img/package2.jpg">
-				<h3>Package Description</h3>
+				<h3>{{{ $packages[2]->description }}} – ${{{ $packages[2]->price }}}</h3>
 					<ul>
-						<li>Price</li>
-						<li>Price</li>
-						<li>Price</li>
+						<li>6 Bakery Lorraine Macarons</li>
+						<li>6 cookies from Lily's Cookies</li>
+						<li>2 juices from Revolucion</li>
+						<li>Dinner for two at Barbaro</li>
+						<li>Dinner for two at The Monterey</li>
+						<li>Coffee for two at Local Coffee</li>
+						<li>One-hour bike ride for two with Bcycle</li>
+						<li>One-day admission for the San Antonio Museum of Art</li>
 					</ul>
 				<span><a role="button" class="btn btn-sm" href="">Send Package</a></span>
 			</div>
 		</div>
-
-@stop
-
-@section('bottomscript')
-<script>
-$('#goToPackages').click(function() {
-	$('#packages').animate({
-		// animate to slide down
-	});
-})
-
-</script>
-
 @stop

af8a4a1a158fff579d387807916e8dd3112de1a8
lesssliemarie <lesssliemarie@gmail.com>
2014-04-14 10:33:38 -0500

    Fixed show details button to account.show

--- app/views/account/show.blade.php
+++ app/views/account/show.blade.php
@@ -73,7 +73,7 @@
 							@if ($order->delivered_at != NULL && $order_packaged_at != NULL)
 							<td>Package Delivered At: {{ $order->delivered_at }} </td>
 							@endif
-							<td style="text-align: center"><a href="{{{ action('OrdersController@show', $user->id) }}}" class="btn btn-sm">See Details</a></td>
+							<td style="text-align: center"><a href="{{{ action('OrdersController@show', $order->id) }}}" class="btn btn-sm">See Details</a></td>
 						</tr>
 						@endforeach
 				@endif

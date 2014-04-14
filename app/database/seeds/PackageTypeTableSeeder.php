c7ca35b7498c2e03230ec1ce43e817fea2bb5058
lesssliemarie <lesssliemarie@gmail.com>
2014-04-14 10:09:54 -0500

    Changed package description on seeder file

--- app/database/seeds/PackageTypeTableSeeder.php
+++ app/database/seeds/PackageTypeTableSeeder.php
@@ -6,17 +6,17 @@ class PackageTypeTableSeeder extends DatabaseSeeder
     {
 
           $packageType1 = new PackageType;
-          $packageType1->description = 'One good, three certificates';
+          $packageType1->description = 'Standard Package';
           $packageType1->price = 25.00;
           $packageType1->save();
 
           $packageType2 = new PackageType;
-          $packageType2->description = 'Two goods, four certificates';
+          $packageType2->description = 'Premium Package';
           $packageType2->price = 45.00;
           $packageType2->save();
 
           $packageType3 = new PackageType;
-          $packageType3->description = 'Three goods, six certificates';
+          $packageType3->description = 'Grand Package';
           $packageType3->price = 65.00;
           $packageType3->save();
 

4ceb0c2508a69315f2e3ecd0c200df74406fec67
lesssliemarie <lesssliemarie@gmail.com>
2014-04-17 14:38:55 -0500

    Stylizing and content editing for about & packages

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -3,11 +3,11 @@
 @section('main-content')
 		<!-- HERO IMAGE -->
 	<div class="hideMain">
-		<div id="hero">
-			<div id="hero-message">
+		<div id="hero" class="col-md-12">
+			<div id="hero-message" class="img-responsive">
 				<h1>The best of local brought to you</h1>
-				<p>Local Care Package is a curated gift service featuring high quality products from local San Antonio businesses, aiming to connect residents to their community and support local business. Know someone who needs some local love?</p>
-				<span><a role="button" class="btn btn-sm" href="{{{ action('HomeController@showPackages') }}}">Send a package</a></button>
+				<p>Local Care Package is a curated gift service featuring high quality products from local San Antonio businesses, aiming to connect residents to their community and support local business. Know someone who needs a little local love?</p>
+				<span><a role="button" class="btn btn-sm" href="{{{ action('HomeController@showPackages') }}}">Send a package today!</a></button>
 			</div>
 		</div>
 		<!-- END HERO -->
@@ -17,24 +17,24 @@
 			<div class="col-md-4 sub-message">
 				<div id="sub-message1">
 					<div class="sub-text" id="sub-text1">
-						<h2>Point 1</h2>
-						<p>Pinterest umami Tonx ethnic. Blue Bottle Brooklyn Odd Future, wolf 90's cray iPhone blog organic mixtape. VHS Helvetica squid scenester locavore fixie, biodiesel yr food truck pickled semiotics.</p>
+						<h2>Community</h2>
+						<p>Help grow our community by giving gifts that create connection, form meaningful relationships, and show what makes our city unique.</p>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-4 sub-message">
 				<div id="sub-message2">
 					<div class="sub-text" id="sub-text2">
-						<h2>Point 2</h2>
-						<p>Irony mustache lo-fi polaroid, meh church-key bitters Etsy meggings tousled Banksy. Hoodie single-origin coffee hella meggings. </p>
+						<h2>Quality</h2>
+						<p>Our packages feature the highest quality of goods and experiences in San Antonio from favorite local businesses, sure to be a special treat for anyone living and playing in the 210.</p>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-4 sub-message">
 				<div id="sub-message3">
 					<div class="sub-text" id="sub-text3">
-						<h2>Point 3</h2>
-						<p>Pour-over meggings bitters church-key iPhone chambray squid distillery post-ironic salvia, disrupt small batch kale chips viral Brooklyn. Tousled food truck Thundercats actually seitan.</p>
+						<h2>Care</h2>
+						<p>We care about our customers, recipients, and businesses and their satisfaction with our products and services. Our ears and eyes are always open, seeking the best experience for everyone.</p>
 					</div>
 				</div>
 			</div>
@@ -55,34 +55,28 @@
 	});
 
 	$('#sub-message1').mouseover(function() {
-		$('#sub-message1').addClass('opacity');
 		$('#sub-text1').fadeIn();
 	});
 
 	$('#sub-message1').mouseleave(function() {
-		$('#sub-message1').removeClass('opacity');
 		$('#sub-text1').fadeOut();
 	});
 
 	$('#sub-message2').mouseover(function() {
-		$('#sub-message2').addClass('opacity');
 		$('#sub-text2').fadeIn();
 	});
 
 	$('#sub-message2').mouseleave(function() {
-		$('#sub-message2').removeClass('opacity');
 		$('#sub-text2').fadeOut();	
 	});
 
 	$('#sub-message3').mouseover(function() {
-		$('#sub-message3').addClass('opacity');
 		$('#sub-text3').fadeIn();
 	});
 
 	$('#sub-message3').mouseleave(function() {
-		$('#sub-message3').removeClass('opacity');
 		$('#sub-text3').fadeOut();	
-	})
+	});
 
 </script>
 @stop

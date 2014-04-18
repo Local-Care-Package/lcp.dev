d4dc4571e97370d219057f212f8351316c76fa50
lesssliemarie <lesssliemarie@gmail.com>
2014-04-18 10:31:08 -0500

    Tweaked "in progress" admin views

--- app/views/vendors.blade.php
+++ app/views/vendors.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.admin-master')
 
 @section('main-content')
-
-<h1>Vendors Page</h1>
-<p>The intent of this page is the following</p>
-<ul>
-  <li>Provide running list of active vendors</li>
-  <li>Add/Delete/Edit Vendors functionality (anticipated in seperate controller action)</li>
-  <li>Provide contact log for vendor relations</li>
-  <li>Vendor payment/inventory request tracking</li>
-</ul>
-
+<div class="row">
+		<div class="col-md-10 centered">
+		<h1><i class="fa fa-cogs"></i> This page is still being worked on <i class="fa fa-cogs"></i></h1>
+	</div>
+	</div>
+	<div class="row">
+		<div class="col-md-10 centered">
+		<h1>Vendors Page</h1>
+			<p><em>The intent of this page is the following:</em></p>
+			<ul>
+			  <li>Provide running list of active vendors</li>
+			  <li>Add/Delete/Edit Vendors functionality (anticipated in seperate controller action)</li>
+			  <li>Provide contact log for vendor relations</li>
+			  <li>Vendor payment/inventory request tracking</li>
+			</ul>
+		</div>
+	</div>
 @stop
